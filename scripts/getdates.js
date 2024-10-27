5d09eae2d8900d2792d1dc42bc129050ee01d6a8
Sione-Uhlig <uhl24001@byui.edu>
2024-10-26 22:10:52 -0500

    woodworking 1.4

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,24 @@
 const year = document.querySelector("#yearjs");
-const today = new Date();
+        const today = new Date();
+        year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 
-year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+        const lastModified = document.lastModified;
+        document.getElementById('lastModified').textContent += lastModified;
 
-const lastModified = document.lastModified;
-document.getElementById('lastModified').textContent += lastModified;
+        // Hamburger control 
+        document.addEventListener('DOMContentLoaded', function() {
+            const hamburger = document.querySelector('.hamburger');
+            const navigation = document.querySelector('.navigation');
+
+            hamburger.addEventListener('click', function() {
+                hamburger.classList.toggle('active');
+                navigation.classList.toggle('active');
+            });
+
+            document.addEventListener('click', function(event) {
+                if (!hamburger.contains(event.target) && !navigation.contains(event.target)) {
+                    hamburger.classList.remove('active');
+                    navigation.classList.remove('active');
+                }
+            });
+        });
